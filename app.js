7853659aac8de0796b97c89d0e62006da205124a
sebilo2010. <kitplayz303@gmail.com>
2026-01-18 11:42:09 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,61 +1,14 @@
-const url = require("url") 
-const http = require("http") 
-const port = 10000
-const fs = require("fs") 
-const process = require("process")
+// 1. Import the Express library
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+// 2. Define what happens when someone visits the home page
+app.get('/', (req, res) => {
+  res.send('hi this is poteto and its working');
+});
 
-http.createServer(async function(req, res) {
-    const urlpath = url.parse(req.url, true) 
-    const parsedpath = urlpath.path
-    let query = urlpath.query
-    const path = parsedpath.split("?")[0]
-    if (path.charAt(path.length - 1) == "?") {
-        query = parsedpath.split("?")[1].replace("?", "")
-    }
-    console.log("Method requested: " + req.method + ", with endpoint " + path);
-    if (req.method == "GET") {
-        if (path == "/") {
-            sendf(res, "index.html");
-        } else if (path == "/status") {
-            res.write("i am... good and healthy :D");
-            res.end();
-        } else if (path == "/games/list") {
-            sendf(res, "games/list.html");
-        } else if (path == "/assets/logo.png") {
-            sendf(res, "assets/logo.png");
-            res.end();
-        } else if (path == "/style.css") {
-            sendf(res, "assets/style.css");
-            res.end();
-        } else if (path == "/games/style.css") {
-            sendf(res, "assets/style.css");
-            res.end();
-        } else if (path == "/games/thumbnail1.jpg") {
-            sendf(res, "assets/thumbnail1.jpg");
-            res.end();
-        } else if (path == "/games/start") {
-            sendf(res, "games/start.html");
-        // 2015 trash sfuff
-        } else if (path == "/home") {
-            sendf(res, "2015/home.html");
-        // other sfuff that work both in 2014 and 2015
-        } else if (path == "/other/bc.png") {
-            sendf(res, "assets/bc.png");
-            res.end();
-        } else if (path == "/mobile-app-upgrades") {
-            sendf(res, "other/mobile-app-upgrades.html");
-        }
-    }
-    if (req.method == "POST") {
-        if (path == "/mobileapi/login") {
-          let userData = GetUserData(WBtest)
-          res.write(JSON.stringify(userData));
-        }
-    }
-}).listen(port);
-
-function sendf(res, file) {
-    res.write(fs.readFileSync(__dirname + "/" + file));
-    res.end();
-}
+// 3. Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
